1dbb86ef68757b6f420b7833af9fd8fa6a3de213
Fr0zen3k <jan.kresic@circuitmess.com>
2025-02-04 12:02:55 +0100

    Added Nevera-Common submodule, initial submodule and dependency setup, dummy feed frame class

---
+++ src/FeedFrame.h
@@ -0,0 +1,10 @@
+#ifndef NEVERA_COMMON_FEEDFRAME_H
+#define NEVERA_COMMON_FEEDFRAME_H
+
+#include <Object/Object.h>
+
+class FeedFrame : public Object {
+    GENERATED_BODY(FeedFrame, Object)
+};
+
+#endif //NEVERA_COMMON_FEEDFRAME_H
