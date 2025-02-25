b483da02e72242fd29be1f0328e04556df1c03ad
Emil Gajšak <emil.gajsak@gmail.com>
2025-02-25 14:12:15 +0100

    Addresses and ports as constants

--- src/CommData.h
+++ src/CommData.h
@@ -3,6 +3,11 @@
 
 #include <Object/Object.h>
 
+static constexpr const char* ControllerIP = "11.0.0.1";
+static constexpr const char* VehicleIP = "11.0.0.2";
+static constexpr const uint16_t UDPPort = 6001;
+static constexpr const uint16_t TCPPort = 6000;
+
 class CommData : public Object {
     GENERATED_BODY(CommData, Object)
 
